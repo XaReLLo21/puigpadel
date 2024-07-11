0588aad21f1433a0ee04351e9d6d413780497ca7
Magnus Haggren <barcelonaguiden@gmail.com>
2024-07-11 13:13:05 +0200

    final

--- src/components/Slutspelet.jsx
+++ src/components/Slutspelet.jsx
@@ -23,19 +23,19 @@ const Slutspel = () => {
       </div>
       <div className='round'>
         <div className='match'>
-          <div className='team-cuatro'>Joel - Joaquim</div>
+          <div className='team'>Joel - Joaquim</div>
           <div className='team-cuatro'>Jaume - Boris</div>
         </div>
         <div className='match'>
           <div className='team-cuatro'>Lluís - Fèlix</div>
-          <div className='team-cuatro'>Albert - Joan</div>
+          <div className='team'>Albert - Joan</div>
         </div>
       </div>
       <div className='round'>
         <div className='match'>
-          <div className='team'>Guanyador</div>
+          <div className='team-cuatro'>Jaume - Boris</div>
 
-          <div className='team'>Guanyador</div>
+          <div className='team-cuatro'>Lluís - Fèlix</div>
         </div>
       </div>
     </div>
