90f122ec9546adead1b9b22c837bafbbcba2c9a9
Magnus Haggren <barcelonaguiden@gmail.com>
2024-10-14 13:50:46 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    __BUILD_TIME__: JSON.stringify(new Date().toISOString()), // Ensure this is double underscores
+    __BUILD_TIME__: JSON.stringify(new Date().toISOString().split('T')[0]), // Get only the date part (YYYY-MM-DD)
   },
 })
