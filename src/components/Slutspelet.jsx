068b75379b8b772058d060bf8dfabe8551e47d1b
Magnus Haggren <barcelonaguiden@gmail.com>
2024-11-16 19:35:22 +0100

    slitspel

--- src/components/Slutspelet.jsx
+++ src/components/Slutspelet.jsx
@@ -4,7 +4,7 @@ const Slutspel = () => {
   // Mock group data
   const groupA = ['Marc | David', 'Equip A2', 'Equip A3', 'Àngel | Jaume']
   const groupB = ['Equip B1', 'Equip B2', 'Equip B3', 'Equip B4']
-  const groupC = ['Equip C1', 'Equip C2', 'Equip C3', 'Equip C4']
+  const groupC = ['Joan | Albert', 'Equip C2', 'Equip C3', 'Equip C4']
   const groupD = [
     'Àngel | Isma',
     'Javi | Magnus',
